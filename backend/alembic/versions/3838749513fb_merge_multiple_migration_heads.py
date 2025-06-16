3208f9b506de50109d31029f3485abd642a4bb52
Cassius P <287ilias@gmail.com>
2025-06-16 12:50:53 +0200

    Fix migrations issues

--- backend/alembic/versions/3838749513fb_merge_multiple_migration_heads.py
+++ backend/alembic/versions/3838749513fb_merge_multiple_migration_heads.py
@@ -1,8 +1,8 @@
-"""Merge node types and crew branches
+"""Merge multiple migration heads
 
-Revision ID: 5825db52a6be
-Revises: 9362a4f8e515, e147fe9936d2
-Create Date: 2025-06-03 02:37:04.186313
+Revision ID: 3838749513fb
+Revises: b3f4c5d6e7f8, e147fe9936d2
+Create Date: 2025-06-16 12:41:29.208823
 
 """
 from typing import Sequence, Union
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '5825db52a6be'
-down_revision: Union[str, None] = ('9362a4f8e515', 'e147fe9936d2') # type: ignore
+revision: str = '3838749513fb'
+down_revision: Union[str, None] = ('b3f4c5d6e7f8', 'e147fe9936d2') #type: ignore
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
